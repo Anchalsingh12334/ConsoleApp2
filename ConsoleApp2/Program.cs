2905b34ba4a66eb24f0b782cf14d0b924a0a65d1
dell <dell@DESKTOP-7AQICT6>
2022-02-21 01:49:09 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			//#Que 1
+			/*int a = 1, b = 3, sum;
+			sum = a + b;
+			Console.WriteLine(sum);
+			@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+
+			//# Que2
+			/*Console.WriteLine(-1 + 4 * 6);
+             Console.WriteLine((35+5)%7);
+             Console.WriteLine(14+-4*6/11);
+             Console.WriteLine(2+15/6*1-7%2); 
+###################################################################################### */
+
+			/*               #Que 3
+				int a = 2, b = 4, temp;
+				temp = a;
+				a = b;
+
+				b = temp;
+				Console.WriteLine("Value of a : " + a);
+				Console.WriteLine("Value of b : "+b);                       */
+			/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+											  # Que 4
+						int a, b;
+						Console.WriteLine("enter the value of 1st number");
+						a = int.Parse(Console.ReadLine());
+						Console.WriteLine("enter the value of 2nd number");
+						b = int.Parse(Console.ReadLine());
+						Console.WriteLine(a + b);
+						Console.WriteLine(a - b);
+						Console.WriteLine(a * b);
+						Console.WriteLine(a / b);   */
+
+			/*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+												   #Que 5
+					double a;
+
+					Console.WriteLine("enter the value in celcius");
+					a = double.Parse(Console.ReadLine());
+					double k = (a + 273.15);
+					Console.WriteLine("The value in kalvin is :" + k);
+					double f = (a * 9 / 5) + 32;
+					Console.WriteLine("The value in fahrenheit is :" + f); 
+		++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+
+			/*Console.WriteLine("enter any string to check for palindrome : ");
+			string a = Console.ReadLine();
+			string b = "";
+			for (int i = a.Length - 1; i >= 0; i--)
+			{
+				b = b + a[i];
+			}
+			if (a == b) Console.WriteLine("palindrome string ");
+			else Console.WriteLine("not a palindrome string ");*/
+
+
+			/*#Que 6
+string a = "ANCHAL";
+Console.WriteLine(a. ToLower());       */
+
+			/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+											 #Que 8
+						string a = "I am going to school";
+						string b = "";
+						for (int i = a.Length - 1; i >= 0; i--)
+						{
+							b = b + a[i];
+						}
+						Console.WriteLine(b);                                */
+			/* # Que 10
+
+                                       Console.WriteLine("enter the no. to test");
+                                      int i = int.Parse(Console.ReadLine());
+                                      if(i%2==0)
+                                       {
+                                      Console.WriteLine("no. is even");
+
+                                       }
+                                          else
+                                            {
+                                          Console.WriteLine("no is odd");
+                                         }               */
+			/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                    #Que 12
+			int i;
+								  for(i=1; i<=10; i++)
+            {
+				Console.WriteLine(i);
+            }             */
+
+			//(((((((((((((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))))))))))))
+/*# Que 13
+
+			int i, sum = 0;
+
+			for (i = 0; i <= 10; i++)
+			{
+				sum = sum + i;
+			}
+			Console.WriteLine(sum);
+			Console.WriteLine();   */
+		}
+ 
+		}
+	}
